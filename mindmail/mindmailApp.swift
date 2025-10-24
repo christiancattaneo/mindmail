722c0dfeca432c8062b5054e315aacd68419e096
Christian Cattaneo <christian.cattaneo@gauntletai.com>
2025-10-23 19:35:05 -0500

    Initial Commit

---
+++ mindmail/mindmailApp.swift
@@ -0,0 +1,17 @@
+//
+//  mindmailApp.swift
+//  mindmail
+//
+//  Created by Christian Cattaneo on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct mindmailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mindmailTests/mindmailTests.swift
@@ -0,0 +1,17 @@
+//
+//  mindmailTests.swift
+//  mindmailTests
+//
+//  Created by Christian Cattaneo on 10/23/25.
+//
+
+import Testing
+@testable import mindmail
+
+struct mindmailTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
